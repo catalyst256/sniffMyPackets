0a2775424c8c03587e4bd830cc84ec4a08d7fb78
catalyst256 <catalyst256@gmail.com>
2013-10-23 13:45:20 +0100

    Added p0f detection, and reverse mapping to IP, tweaked .conf file to allow setting of p0f location

--- src/sniffMyPackets/transforms/__init__.py
+++ src/sniffMyPackets/transforms/__init__.py
@@ -10,6 +10,8 @@ __maintainer__ = 'catalyst256'
 __email__ = 'catalyst256@gmail.com'
 __status__ = 'Development'
 __all__ = [
+    'opsys2ip',
+    'pcap2opsys',
     'opensharklau',
     'decodeas',
     'extractalert',
