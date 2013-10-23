cc5a586e95b307b60552a2824100c36fac59874b
catalyst256 <catalyst256@gmail.com>
2013-10-23 15:05:57 +0100

    Added some more MAC address transforms, search pcap for MAC and resolve back to IP

--- src/sniffMyPackets/transforms/macaddr2ip.py
+++ src/sniffMyPackets/transforms/macaddr2ip.py
@@ -24,31 +24,29 @@ __all__ = [
 
 #@superuser
 @configure(
-    label='L4 - IP address to MAC address [SmP]',
-    description='Returns MAC address from IPv4 Address',
-    uuids=[ 'sniffMyPackets.v2.IPAddr_2_MACaddr' ],
-    inputs=[ ( 'sniffMyPackets', IPv4Address ) ],
+    label='L2 - Extract IPv4 from MAC [SmP]',
+    description='Pulls out IPv4 Addresses that match the MAC address',
+    uuids=[ 'sniffMyPackets.v2.macaddr_2_ip' ],
+    inputs=[ ( 'sniffMyPackets', MacAddress ) ],
     debug=False
 )
 def dotransform(request, response):
-    
-    s_ip = request.value
-    try:
-        pcap = request.fields['pcapsrc']
-    except:
-        return response + UIMessage('Sorry this isnt a SmP IP Address')
-    
-    mac_list = []
+
+    s_mac = request.value
+    ip_list = []
+    pcap = request.fields['pcapsrc']
 
     pkts = rdpcap(pcap)
-    for x in pkts:
-        if x.haslayer(IP) and s_ip == x[IP].src:
-            mac = x[Ether].src
-            if mac not in mac_list:
-                mac_list.append(mac)
 
-    for x in mac_list:
-        e = MacAddress(x)
+    for x in pkts:
+        if x.haslayer(IP):
+            if s_mac == x[Ether].src:
+                s_ip = x[IP].src
+                if s_ip not in ip_list:
+                    ip_list.append(s_ip)
+
+    for t in ip_list:
+        e = IPv4Address(t)
         e += Field('pcapsrc', pcap, displayname='Original pcap File')
         response += e
     return response
